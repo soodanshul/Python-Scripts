4e1f1d2e3f03895ae91d30d4c1c701e939e3d9b7
soodanshul <soodanshul9210@gmail.com>
2020-11-05 00:45:24 -0800

    Update ShowConfig.py

--- ShowConfig.py
+++ ShowConfig.py
@@ -1,11 +1,13 @@
-from netmiko import ConnectHandler # Module which enables SSH connection
+#basic pyhton script to show the running-configuration of Cisco Devices
+
+from netmiko import ConnectHandler      # Module which enables SSH connection
 
 def show_conf():
 
     platform = 'cisco_ios'
     host = input('Enter the HostName or IP Address: ')
-    username = input('Enter the Login UserName: ') # edit to reflect
-    password = input('Enter the password: ') # edit to reflect
+    username = input('Enter the Login UserName: ')
+    password = input('Enter the password: ') 
     device = ConnectHandler(device_type=platform, ip=host, username=username, password=password)
     output = device.send_command('show running-config')
     print(output)
